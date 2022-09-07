74db2947186df96c04537d338fa203bac610f745
John Gunter <JLGunter108@gmail.com>
2022-09-07 01:58:55 -0700

    second commit

--- src/script.js
+++ src/script.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three'
 import * as dat from 'dat.gui'
+import { DoubleSide } from 'three';
 
 let previousShadowMap = false;
 
@@ -12,6 +13,7 @@ const textureLoader = new THREE.TextureLoader()
 
 const divetTexture = textureLoader.load('textures/normal-divets.jpg')
 const waterTexture = textureLoader.load('textures/water.jpg');
+const roughTexture = textureLoader.load('textures/normal-pumice.jfif');
 
 // Debug
 
@@ -26,33 +28,43 @@ const scene = new THREE.Scene()
 // Objects
 const geometry = new THREE.SphereGeometry( .5, 64, 64);
 const sunGeometry = new THREE.SphereGeometry(.02,16,8);
+const shadowBoxGeometry = new THREE.PlaneGeometry( 9,1.5 );
 
 // Materials
 
-const material = new THREE.MeshStandardMaterial({color: 0x000fd0, normalMap:divetTexture})
+const material = new THREE.MeshPhongMaterial({color: 0xffffff, envMap:divetTexture})
+material.transparent = true;
+material.blending = THREE.AdditiveBlending;
 const sunMaterial = new THREE.MeshStandardMaterial( {
     emissive: 0xffffee,
     emissiveIntensity: 1,
     color: 0x000000
 });
+const shadowBoxMaterial = new THREE.MeshStandardMaterial({ color: 0xffffff, normalMap: roughTexture});
+shadowBoxMaterial.side = DoubleSide;
 
 // Mesh
 const sphere = new THREE.Mesh(geometry,material)
+sphere.castShadow = true;
 scene.add(sphere);
+const shadowBox = new THREE.Mesh(shadowBoxGeometry, shadowBoxMaterial)
+shadowBox.position.z = -1
+shadowBox.receiveShadow = true;
+scene.add(shadowBox);
 
 // Lights
 
-const pointLight = new THREE.PointLight(0xff0000, 10)
-pointLight.position.set(-4.59,-30,-30)
-scene.add(pointLight)
+// const pointLight = new THREE.PointLight(0xff0000, 10)
+// pointLight.position.set(10,-10,5)
+// scene.add(pointLight)
 
 // const pointLight2 = new THREE.PointLight(0xffffff, 10)
 // pointLight2.position.set(-19.14,0.71,-20)
 // scene.add(pointLight2)
 
-const sunLight = new THREE.PointLight(0xffeeee, 10, 100, 2);
+const sunLight = new THREE.PointLight(0xffeeee, 7, 100, 2);
 sunLight.add( new THREE.Mesh(sunGeometry,sunMaterial));
-sunLight.position.set(-1, .6, 0);
+sunLight.position.set(2.5, 0, -0.2);
 sunLight.castShadow = true;
 const sunLightObj = new THREE.Object3D();
 sunLightObj.add(sunLight);
@@ -76,10 +88,8 @@ window.addEventListener('resize', () =>
 })
 
 // Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.x = 0
-camera.position.y = 0
-camera.position.z = 3
+const camera = new THREE.PerspectiveCamera(95, sizes.width / sizes.height, 0.1, 100)
+camera.position.set(0,0,2.5)
 scene.add(camera)
 
 
@@ -96,44 +106,47 @@ renderer.toneMapping = THREE.ReinhardToneMapping;
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
+
+
+
 /**
  * Animate
  */
 
-document.addEventListener('mousemove', onDocumentMouseMove)
-
 let mouseX = 0;
 let mouseY = 0;
 
-let targetX = 0;
-let targetY = 0;
+document.addEventListener('mousemove', onDocumentMouseMove)
 
 const windowX = window.innerWidth / 2;
 const windowY = window.innerHeight / 2;
 
 function onDocumentMouseMove(event) {
-    mouseX = (event.clientX - windowX)
-    mouseY = (event.clientY - windowY)
+    mouseX = ( event.clientX - windowX ) / 5000;
+    mouseY = ( event.clientY - windowY ) / 1000;
 }
 
 const clock = new THREE.Clock()
 
 const tick = () =>
 {
-    targetX = mouseX * .001
-    targetY = mouseY * .001
 
     const elapsedTime = clock.getElapsedTime()
 
+    camera.position.x += ( mouseX - camera.position.x ) * .05;
+    camera.position.y += ( - mouseY - camera.position.y ) * .05;
+    camera.lookAt(scene.position)
     // Update objects
     sphere.rotation.y = .2 * elapsedTime
-    sunLight.rotateY(0.01);
-    sunLightObj.rotateY(0.01);
+    sunLight.rotateY(-0.008);
+    sunLightObj.rotateY(-0.008);
     // Render
     renderer.render(scene, camera)
 
     // Call tick again on the next frame
     window.requestAnimationFrame(tick)
+
+
 }
 
 tick()
